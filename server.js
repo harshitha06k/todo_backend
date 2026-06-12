b3bbb90680dc205a69730e0fa1fddb4eb64841d5
harshitha06k <harshitha06k@gmail.com>
2026-06-12 12:46:04 +0530

    update

--- server.js
+++ server.js
@@ -2,17 +2,60 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+const SECRET = "hello123"
 app.listen(process.env.PORT,()=>{console.log("server started")})
 mongoose.connect(process.env.MONGO_URI)
+const userSchema = mongoose.Schema({
+    name:"String",
+    email:"String",
+    password: "String",
+})
+const userModel = mongoose.model("User", userSchema)
 
 const listSchema = mongoose.Schema({
     task:String
 })
 const listModel = mongoose.model("List",listSchema)
+
+app.post("/users/register",async (req,res)=>{
+    console.log(req.body)
+    const hashpassword = await bcrypt.hash(req.body.password,10)
+     req.body.password = hashpassword
+     const user = await userModel.create(req.body)
+     res.json(user)
+})
+
+app.post("/users/login", async (req, res) => {
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email })
+    if(user) {
+        const chkPassword = await bcrypt.compare(password,user.password)
+        if(chkPassword) {
+            const obj = {
+                id: user._id,
+                name: user.name,
+                email: user.email
+        }
+        const token = jwt.sign(obj,SECRET,{expiresIn: "1h"})
+        res.json({success: true,...obj,token})
+        }
+        else {
+             res.json({success: false,message: "Invalid Password"})
+
+        }
+
+    }
+    else {
+         res.json({success: false,message: "User not found"})
+    }
+})
+
 
 app.get("/lists",async (req,res)=>{
     const lists = await listModel.find()
